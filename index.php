f257a5ab8a12a132dc463cfc8149d469ec4e3f66
weiguoheng <weiguoheng1119@163.com>
2021-12-30 16:07:44 +0800

    Update index.php
    php加密

--- index.php
+++ index.php
@@ -3,5 +3,8 @@ $iv = '1234567890123412';
 $key = 'jadetangshitv123';
 $data = '6AWg5Ab0WlKNd47evmMKo6v7S1Yw4Uzx7z0TU8PDN/dExo8weeI7jzHTG1OnXPifxeDOErwLk9ZeTbY3YVz2oajcmQiIWHMJ3so91IyTFoo=';
 $decodeStr = openssl_decrypt(base64_decode($data), "AES-128-CBC",$key,OPENSSL_RAW_DATA, $iv);
-
-var_dump($decodeStr);
+var_dump($decodeStr);//解密打印js加密数据
+$encrypt_data = openssl_encrypt($decodeStr, "AES-128-CBC",$key,OPENSSL_RAW_DATA, $iv);
+var_dump($encrypt_data);//php加密数据
+$decodeStr = openssl_decrypt($encrypt_data, "AES-128-CBC",$key,OPENSSL_RAW_DATA, $iv);
+var_dump($decodeStr);//php解密数据
